7179ccc8e41007f8499949ef0d6c2c0272b9a477
Konservator-mn <fryb08179@gmail.com>
2018-09-06 10:02:59 +0300

    2

--- test/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-    var stream = fs.createReadStream('./recycling-final-optimized.html');
-    stream.on('data', function (data) {
-        res.end(data);
-    });
-}).listen(3000);
